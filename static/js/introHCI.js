a64b9b9f1bf64510d27e37d24a361c61605ef2d1
David Jasuan <djasuan@ucsd.edu>
2020-01-16 12:54:40 -0800

    added javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,17 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+            $("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e){
+    //prevent the page from reloading
+    e.preventDefault();
+    //in an event handler, $(this)refers to
+    //the object that triggered the event
+    $(this).css("background-color", "#7fff00");
+}
+
+
+
+
